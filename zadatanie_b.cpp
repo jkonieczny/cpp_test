11d3c2977a3f48e911a7ec9937945c5cb7308eff
Jan Konieczny <jkonieczny@gmail.com>
2012-04-23 12:53:56 +0100

    zadanie 3

--- zadatanie_b.cpp
+++ zadatanie_b.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+#define ALPHABET_LETTERS 26
 /**
  *  zadanie 2 (wyswietlanie tablicy
  * @param name nazwa tablicy do wyswietlenia
@@ -11,15 +12,47 @@ using namespace std;
  */
 void print_array(string name,int array[],int size);
 
+/**
+ * zadanie 1
+ * wygeneruj tablice  o rozmiarze 'size' z kolejnymi liczbami naturalnymi
+ * @param size rozmiar tablicy
+ * @return array pointer
+ */
 int* generate_array(int size);
 
 
+void generate_alphabet(char* array);
+
 int main()
 {
+    int size;
+    char C[ALPHABET_LETTERS];
     // 1
     cout << "Podaj rozmiar tablicy z liczbami naturalnymi: ";
+    cin >> size;
+    int* A = generate_array(size);
+    cout << "\nTablica z " << size << " liczbami naturalnymi (wliczajac 0): \n";
     
+    // 2
+    print_array("A",A,size);
+    
+    
+    // 3
+    generate_alphabet(C);
+    cout << "Litery alfabetu:\n";
+    for(int i=0;i<ALPHABET_LETTERS;i++){
+        cout << (char)C[i] << " ";
+    }
     return 0;      
+}
+
+void generate_alphabet(char* array)
+{
+    int i=0;
+    for(char a='A';a<='Z';a++){
+        array[i] = a;
+        i++;
+    }
 }
 
 void print_array(string name,int array[],int size)
