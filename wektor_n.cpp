33c251d45b18aff7bdbce2ece7f849bbf0aa17a1
Jan Konieczny <jkonieczny@gmail.com>
2012-06-12 12:28:16 +0100

    naprawiony konstruktor kopiujacy + dodane numery podpunktow w komentarzach

--- wektor_n.cpp
+++ wektor_n.cpp
@@ -8,6 +8,8 @@ class WektorN {
 
 public:
 	/**
+	 *  a)
+	 * 
 	 * Konstruktor
 	 * @param l Label/Etykieta
 	 * @param size Wymiar przestrzeni
@@ -15,29 +17,48 @@ public:
 	 */
 	WektorN(string l, int size, int default_value = 3);
 	/**
+	 * b)
+	 * 
 	 * Kontstruktor kopiujacy 
 	 * @param v
 	 */
 	WektorN(const WektorN &v);
+
 	/**
-	 *  przeladowany operator '<<'
-	 */
-	friend ostream& operator<<(ostream &out, WektorN &v);
-	/**
+	 * c)
+	 * 
 	 * Destruktor
 	 */
 	~WektorN();
+
 	/**
+	 * d)
+	 * 
 	 * Ustaw i-ta wspolrzedna wektora na wartosc W
 	 * @param i
 	 * @param w
 	 */
 	void ustaw(int i, int w);
 
+
 	/**
+	 * e)
+	 * 
 	 *  Przeladowany operator '+'
 	 */
 	friend WektorN operator+(const WektorN &v1, const WektorN &v2);
+
+
+	/**
+	 * f)
+	 * 
+	 *  przeladowany operator '<<'
+	 */
+	friend ostream& operator<<(ostream &out, WektorN &v);
+
+
+
+
 };
 
 WektorN operator+(const WektorN &v1, const WektorN &v2) {
@@ -67,6 +88,7 @@ WektorN::~WektorN() {
 WektorN::WektorN(const WektorN& v) {
 	this->label = v.label;
 	this->array = new int[v.size];
+	this->size = v.size;
 	for (int i = 0; i < v.size; i++) {
 		this->array[i] = v.array[i];
 	}
@@ -93,23 +115,39 @@ ostream& operator<<(ostream &out, WektorN &v) {
 	out << "]";
 }
 
+
+// g)
+
 int main() {
 
+	// a)
 	WektorN v1("pierwszy", 4);
 	WektorN v2("drugi", 7, 10);
 	WektorN v3("trzeci", 4, 8);
+	// b)
+	WektorN vc(v2);
+	// e)
 	WektorN vs = v1 + v3;
+
+	// f)
 	cout << "\nv1: " << v1;
 	cout << "\nv2: " << v2;
 	cout << "\nv3: " << v3;
 
+	// d)
 	v2.ustaw(3, 666);
-	cout << "\nPo ustawieniu wspolrzednej '3' na '666':   " << v2;
+	cout << "\nWektor v2 Po ustawieniu wspolrzednej '3' na '666':   " << v2;
+
+
+	cout << "\nKopia wektora v2 sprzed zmiany wspolrzednej nr. 3:   " << vc;
+
 
 	cout << "\nSuma v1+v3 =  " << vs;
 
 	cout << "\nProbojemy dodac wektory o roznych rozmiarach: \n";
 
+
+	// e) exception
 	try {
 		WektorN vError = v1 + v2;
 		cout << "\nSuma v1+v2  (nie powinno tutaj dojsc) =  " << vError;
